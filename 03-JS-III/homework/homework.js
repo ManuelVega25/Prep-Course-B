f7314064b2a7bcf3b790b1ec32c88b398bc00ed7
ManuelVega25 <mvegamontesinos@gmail.com>
2022-09-25 22:32:28 -0500

    HW03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,52 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acum = 0;
+    for (var i=1; i<=10; i++){
+      acum = acum + i;
+      
+    }
+    return acum;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var paresEncontrados = [];
+  for(var i=0; i<array.length; i++){
+    if (array[i] % 2 === 0){
+      paresEncontrados.push(array[i]);
+    }
+  }
+  return paresEncontrados
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var result = array.map(function(valor){
+    return valor ** 2;
+  });
+   return result
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+   var suma = array.reduce(function (acum, valor) {
+    return acum + valor;
+  });
+   return suma
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+// return  num.toString().length
+return String(num).length
 }
   
   
